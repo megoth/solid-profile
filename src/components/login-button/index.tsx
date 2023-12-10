90905fb064ff7340997dfaf545dc15750dd6687e
Arne Hassel <arne.hassel@capgemini.com>
2023-12-10 04:39:23 +0100

    Adding some styles to login and logout button

--- src/components/login-button/index.tsx
+++ src/components/login-button/index.tsx
@@ -1,13 +1,15 @@
-import {useContext} from "react";
+import {HTMLAttributes, useContext} from "react";
 import ModalContext from "../../hooks/use-modal/context.tsx";
 import LoginButtonModal from "./modal";
+import {clsx} from "clsx";
 
-export default function LoginButton() {
+export default function LoginButton({className, ...props}: HTMLAttributes<HTMLButtonElement>) {
     const {setModal} = useContext(ModalContext);
-    return <>
-        <button className="button is-small" onClick={() => setModal(<LoginButtonModal />)}>
-            <span className="icon is-small"><img src="/solid.svg" alt="Solid logo"/></span>
-            <span>Log in</span>
+    return (
+        <button
+            className={clsx("button is-small is-primary is-light", className)}
+            onClick={() => setModal(<LoginButtonModal/>)} {...props}>
+            Log in
         </button>
-    </>
+    )
 }
--- src/components/logout-button/index.tsx
+++ src/components/logout-button/index.tsx
@@ -5,6 +5,6 @@ import {clsx} from "clsx";
 export default function LogoutButton({className, ...props}: HTMLAttributes<HTMLButtonElement>) {
     const {logout} = useSolidAuth();
     return (
-        <button className={clsx("button is-small", className)} onClick={logout} {...props}>Log out</button>
+        <button className={clsx("button is-small is-danger is-light", className)} onClick={logout} {...props}>Log out</button>
     )
 }
